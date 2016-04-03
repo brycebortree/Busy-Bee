172443bd2a0641428620b1392803cbe53b7965b3
brytree <brycebortree@gmail.com>
2016-04-03 12:57:59 -0700

    found font + updated background image, clickable buttons that fade out

---
+++ script.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+  var roundCount = 0;
+
+  $('.fader').on('click', function(){
+    $(this).parent().fadeOut();
+  })
+
+  $('#learn').on('click', function(){
+    console.log('learn more');
+  });
+
+  $('#startGame').on('click', function(){
+    console.log("game started");
+    //GENERATE BOARD function
+    //IF winner
+    counter++
+    //else
+    //GENERATE BOARD function while WINNER <7
+    //DISPLAY WINNER function
+
+  });
+
+//GENERATE BOARD function
+var GenerateBoard = function(){}
+
+//GENERATE BIG function
+var GenerateBig = function() {}
+
+//WINNER function
+var winner = function(){}
+
+//DISPLAY WINNER function (sweet alert?)
+//prompt GENERATE BIG function
+var displayWinner = function() {}
+
+image array
+var image = [
+{
+  desc: 'Feverfew', photo: 'images/n-feverfew.jpg',
+  desc: 'Pitcher Plant', photo: 'images/n-pitcher-plant.jpg',
+  desc: 'Wormwood', photo: 'images/n-wormwood.jpg',
+  desc: 'Apple Tree', photo: 'images/y-apple.jpg',
+  desc: 'Basil', photo: 'images/y-basil.jpg',
+  desc: 'Cherry', photo: 'images/y-cherry.jpg',
+  desc: 'Hyssop', photo: 'images/y-hyssop.jpg',
+  desc: 'Lavendar', photo: 'images/y-lavendar.jpg',
+  desc: 'Rosemary', photo: 'images/y-rosemary.jpg',
+  desc: 'Snapdragon', photo: 'images/y-snapdragon.jpg'
+}];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
